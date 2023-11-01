ef9f72904618a9f98f95feed22115410652b7f7a
Filip <148322224+FillipBT@users.noreply.github.com>
2023-11-01 10:27:49 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,55 @@
 function myFunction() {
     var x = document.getElementById("text");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
       x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
   }
+
+  function myFunction2() {
+    var x = document.getElementById("text2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  function myFunction3() {
+    var x = document.getElementById("text3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunction4() {
+    var x = document.getElementById("text4");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction5() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction5() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
