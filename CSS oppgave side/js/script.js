c95f7e3e685bd895485c0a8448b6c6a0d5e3e0be
Filip <148322224+FillipBT@users.noreply.github.com>
2023-11-03 11:12:42 +0100

    Add files via upload

---
+++ CSS oppgave side/js/script.js
@@ -0,0 +1,45 @@
+function myFunction() {
+    var x = document.getElementById("text");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("text2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  function myFunction3() {
+    var x = document.getElementById("text3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunction4() {
+    var x = document.getElementById("text4");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunction5() {
+    var x = document.getElementById("text5");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
